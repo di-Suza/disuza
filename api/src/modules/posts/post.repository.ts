3451d975d19d8b79c2a846f5ae58f8cf4b9f8b2c
Sujal Rajput <rajputsujal992@gmail.com>
2026-07-07 04:30:17 +0530

    feat(api): add comments module

--- api/src/modules/posts/post.repository.ts
+++ api/src/modules/posts/post.repository.ts
@@ -29,6 +29,20 @@ class PostRepository {
     return PostModel.findOne({ _id: postId, ...visiblePostQuery }).populate('user', 'profilePicture userName headline');
   }
 
+  findVisibleCommentTarget(postId: string | Types.ObjectId) {
+    return PostModel.findOne({ _id: postId, ...visiblePostQuery })
+      .select('user settings')
+      .lean();
+  }
+
+  incrementCommentsCount(postId: string | Types.ObjectId, delta: number) {
+    return PostModel.findOneAndUpdate(
+      { _id: postId, ...visiblePostQuery },
+      { $inc: { 'counts.comments': delta } },
+      { new: true },
+    );
+  }
+
   findDashboardPosts(userId: string | Types.ObjectId, page: number, limit: number) {
     return PostModel.find({ user: userId, ...visiblePostQuery })
       .sort({ createdAt: -1 })
